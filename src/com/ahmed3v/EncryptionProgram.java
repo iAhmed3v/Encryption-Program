63dce552c9de7c0f51d970624af8205e1aba3b01
iAhmed3v <devahmdali@gmail.com>
2021-11-13 11:28:13 +0300

    Initial commit

---
+++ src/com/ahmed3v/EncryptionProgram.java
@@ -0,0 +1,164 @@
+package com.ahmed3v;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class EncryptionProgram {
+
+    private Scanner scanner;
+    private Random random;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char character;
+    private String line;
+    private char[] letters;
+    
+    EncryptionProgram() {
+
+        scanner = new Scanner(System.in);
+        random = new Random();
+        list = new ArrayList<>();
+        shuffledList = new ArrayList<>();
+        character = ' ';
+
+        newKey();
+        askQuestion();
+    }
+
+    private void askQuestion() {
+
+        while (true){
+            System.out.println("***********************************************************");
+            System.out.println("What do you want to do?");
+            System.out.println("Enter 'N' = newKey, 'G' = getKey, 'E' = encrypt, 'D' = decrypt, 'Q' = quit");
+
+            char response = Character.toUpperCase(scanner.nextLine().charAt(0));
+
+            switch (response){
+
+                case 'N':
+                    newKey();
+                    break;
+
+                case 'G':
+                    getKey();
+                    break;
+
+                case 'E':
+                    encrypt();
+                    break;
+
+                case 'D':
+                    decrypt();
+                    break;
+
+                case 'Q':
+                    quit();
+                    break;
+
+                default:
+                    System.out.println("This is a valid answer!");
+            }
+        }
+    }
+
+    private void newKey() {
+
+        //clear the char and lists before generating any new keys.
+        character = ' ';
+        list.clear();
+        shuffledList.clear();
+
+        for (int i = 32; i < 127; i++){
+
+            list.add(character);
+            character++;
+        }
+
+        shuffledList = new ArrayList<>(list);
+
+        Collections.shuffle(shuffledList);
+
+        System.out.println("A new key has been generated!");
+    }
+
+    private void getKey() {
+
+        System.out.println("Key: ");
+
+        for (Character x : list){
+            System.out.print(x);
+        }
+        System.out.println();
+
+        for (Character x : shuffledList){
+            System.out.print(x);
+        }
+        System.out.println();
+    }
+
+    private void encrypt() {
+
+        System.out.println("Enter a message to get encrypted: ");
+
+        String message = scanner.nextLine();
+
+        letters = message.toCharArray();
+
+        for (int i = 0; i < letters.length; i++){
+
+            for (int j = 0; j < list.size(); j++){
+
+                if (letters[i] == list.get(j)){
+
+                    letters[i] = shuffledList.get(j);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Message Encrypted: ");
+
+        for (char x : letters){
+
+            System.out.print(x);
+        }
+        System.out.println();
+    }
+
+    private void decrypt() {
+
+        System.out.println("Enter a message to get decrypted: ");
+
+        String message = scanner.nextLine();
+
+        letters = message.toCharArray();
+
+        for (int i = 0; i < letters.length; i++){
+
+            for (int j = 0; j < shuffledList.size(); j++){
+
+                if (letters[i] == shuffledList.get(j)){
+
+                    letters[i] = list.get(j);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Message Decrypted: ");
+
+        for (char x : letters){
+
+            System.out.print(x);
+        }
+        System.out.println();
+    }
+
+    private void quit() {
+        System.out.println("Thanks, have a nice day!");
+        System.exit(0);
+    }
+}
---
+++ src/com/ahmed3v/Main.java
@@ -0,0 +1,10 @@
+package com.ahmed3v;
+
+//Java Encryption example:
+public class Main {
+
+    public static void main(String[] args) {
+
+        EncryptionProgram encryptionProgram = new EncryptionProgram();
+    }
+}
